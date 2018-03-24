9e869340d8ff86bed2bb6cc691916aeb0370f153
José Carlos Santiago <santiago.jose@outlook.com>
2018-03-23 21:18:02 -0600

    Ingresando Form de Cliente

--- ProyectoFinal/ProyectoFinal/Form1.Designer.cs
+++ ProyectoFinal/ProyectoFinal/Form1.Designer.cs
@@ -29,13 +29,35 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            DevExpress.Utils.SuperToolTip superToolTip1 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem1 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem1 = new DevExpress.Utils.ToolTipItem();
+            DevExpress.Utils.SuperToolTip superToolTip2 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem2 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem2 = new DevExpress.Utils.ToolTipItem();
+            DevExpress.Utils.SuperToolTip superToolTip3 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem3 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem3 = new DevExpress.Utils.ToolTipItem();
+            DevExpress.Utils.SuperToolTip superToolTip4 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem4 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem4 = new DevExpress.Utils.ToolTipItem();
             this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.btnNewclient = new DevExpress.XtraBars.BarButtonItem();
+            this.btnEditclient = new DevExpress.XtraBars.BarButtonItem();
+            this.btnDelclient = new DevExpress.XtraBars.BarButtonItem();
+            this.btnShowclient = new DevExpress.XtraBars.BarButtonItem();
             this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
             this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPageclientes = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.gridControlClient = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlClient)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // ribbonControl1
@@ -43,15 +65,89 @@
             this.ribbonControl1.ExpandCollapseItem.Id = 0;
             this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
             this.ribbonControl1.ExpandCollapseItem,
-            this.barButtonItem1});
+            this.barButtonItem1,
+            this.btnNewclient,
+            this.btnEditclient,
+            this.btnDelclient,
+            this.btnShowclient});
             this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
-            this.ribbonControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.ribbonControl1.MaxItemId = 4;
+            this.ribbonControl1.MaxItemId = 8;
             this.ribbonControl1.Name = "ribbonControl1";
             this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
-            this.ribbonPage1});
+            this.ribbonPage1,
+            this.ribbonPageclientes});
             this.ribbonControl1.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2010;
-            this.ribbonControl1.Size = new System.Drawing.Size(1137, 219);
+            this.ribbonControl1.Size = new System.Drawing.Size(758, 143);
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "Testing";
+            this.barButtonItem1.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.Glyph")));
+            this.barButtonItem1.Id = 3;
+            this.barButtonItem1.Name = "barButtonItem1";
+            this.barButtonItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem1_ItemClick);
+            // 
+            // btnNewclient
+            // 
+            this.btnNewclient.Caption = "Nuevo";
+            this.btnNewclient.Glyph = ((System.Drawing.Image)(resources.GetObject("btnNewclient.Glyph")));
+            this.btnNewclient.Id = 4;
+            this.btnNewclient.Name = "btnNewclient";
+            this.btnNewclient.RibbonStyle = ((DevExpress.XtraBars.Ribbon.RibbonItemStyles)(((DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large | DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithText) 
+            | DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithoutText)));
+            toolTipTitleItem1.Text = "Nuevo Cliente";
+            toolTipItem1.LeftIndent = 6;
+            toolTipItem1.Text = "Añade un nuevo cliente al registro.";
+            superToolTip1.Items.Add(toolTipTitleItem1);
+            superToolTip1.Items.Add(toolTipItem1);
+            this.btnNewclient.SuperTip = superToolTip1;
+            this.btnNewclient.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnNewclient_ItemClick);
+            // 
+            // btnEditclient
+            // 
+            this.btnEditclient.Caption = "Editar";
+            this.btnEditclient.Glyph = ((System.Drawing.Image)(resources.GetObject("btnEditclient.Glyph")));
+            this.btnEditclient.Id = 5;
+            this.btnEditclient.Name = "btnEditclient";
+            this.btnEditclient.RibbonStyle = ((DevExpress.XtraBars.Ribbon.RibbonItemStyles)(((DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large | DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithText) 
+            | DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithoutText)));
+            toolTipTitleItem2.Text = "Editar";
+            toolTipItem2.LeftIndent = 6;
+            toolTipItem2.Text = "Edita al cliente actualmente seleccionado";
+            superToolTip2.Items.Add(toolTipTitleItem2);
+            superToolTip2.Items.Add(toolTipItem2);
+            this.btnEditclient.SuperTip = superToolTip2;
+            // 
+            // btnDelclient
+            // 
+            this.btnDelclient.Caption = "Eliminar";
+            this.btnDelclient.Glyph = ((System.Drawing.Image)(resources.GetObject("btnDelclient.Glyph")));
+            this.btnDelclient.Id = 6;
+            this.btnDelclient.Name = "btnDelclient";
+            this.btnDelclient.RibbonStyle = ((DevExpress.XtraBars.Ribbon.RibbonItemStyles)(((DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large | DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithText) 
+            | DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithoutText)));
+            toolTipTitleItem3.Text = "Eliminar";
+            toolTipItem3.LeftIndent = 6;
+            toolTipItem3.Text = "Elimina al cliente actualmente seleccionado";
+            superToolTip3.Items.Add(toolTipTitleItem3);
+            superToolTip3.Items.Add(toolTipItem3);
+            this.btnDelclient.SuperTip = superToolTip3;
+            // 
+            // btnShowclient
+            // 
+            this.btnShowclient.Caption = "Mostrar";
+            this.btnShowclient.Glyph = ((System.Drawing.Image)(resources.GetObject("btnShowclient.Glyph")));
+            this.btnShowclient.Id = 7;
+            this.btnShowclient.Name = "btnShowclient";
+            this.btnShowclient.RibbonStyle = ((DevExpress.XtraBars.Ribbon.RibbonItemStyles)(((DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large | DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithText) 
+            | DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithoutText)));
+            toolTipTitleItem4.Text = "Mostrar";
+            toolTipItem4.LeftIndent = 6;
+            toolTipItem4.Text = "Muestra los registros almacenados sobre los clientes";
+            superToolTip4.Items.Add(toolTipTitleItem4);
+            superToolTip4.Items.Add(toolTipItem4);
+            this.btnShowclient.SuperTip = superToolTip4;
+            this.btnShowclient.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnShowclient_ItemClick);
             // 
             // ribbonPage1
             // 
@@ -66,38 +162,66 @@
             this.ribbonPageGroup1.Name = "ribbonPageGroup1";
             this.ribbonPageGroup1.Text = "ribbonPageGroup1";
             // 
+            // ribbonPageclientes
+            // 
+            this.ribbonPageclientes.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup2});
+            this.ribbonPageclientes.Name = "ribbonPageclientes";
+            this.ribbonPageclientes.Text = "Clientes";
+            // 
+            // ribbonPageGroup2
+            // 
+            this.ribbonPageGroup2.ItemLinks.Add(this.btnNewclient);
+            this.ribbonPageGroup2.ItemLinks.Add(this.btnEditclient);
+            this.ribbonPageGroup2.ItemLinks.Add(this.btnDelclient);
+            this.ribbonPageGroup2.ItemLinks.Add(this.btnShowclient);
+            this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+            this.ribbonPageGroup2.Text = "Administrar";
+            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 219);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 143);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1137, 307);
+            this.dataGridView1.Size = new System.Drawing.Size(758, 217);
             this.dataGridView1.TabIndex = 1;
             // 
-            // barButtonItem1
+            // gridControlClient
             // 
-            this.barButtonItem1.Caption = "Testing";
-            this.barButtonItem1.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.Glyph")));
-            this.barButtonItem1.Id = 3;
-            this.barButtonItem1.Name = "barButtonItem1";
-            this.barButtonItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem1_ItemClick);
+            this.gridControlClient.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControlClient.Location = new System.Drawing.Point(0, 143);
+            this.gridControlClient.MainView = this.gridView1;
+            this.gridControlClient.MenuManager = this.ribbonControl1;
+            this.gridControlClient.Name = "gridControlClient";
+            this.gridControlClient.Size = new System.Drawing.Size(758, 217);
+            this.gridControlClient.TabIndex = 3;
+            this.gridControlClient.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControlClient;
+            this.gridView1.Name = "gridView1";
             // 
             // Form1
             // 
             this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.False;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1137, 526);
+            this.ClientSize = new System.Drawing.Size(758, 360);
+            this.Controls.Add(this.gridControlClient);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.ribbonControl1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Ribbon = this.ribbonControl1;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlClient)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,6 +234,14 @@
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
         private DevExpress.XtraBars.BarButtonItem barButtonItem1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private DevExpress.XtraBars.BarButtonItem btnNewclient;
+        private DevExpress.XtraBars.BarButtonItem btnEditclient;
+        private DevExpress.XtraBars.BarButtonItem btnDelclient;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPageclientes;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+        private DevExpress.XtraBars.BarButtonItem btnShowclient;
+        private DevExpress.XtraGrid.GridControl gridControlClient;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
     }
 }
 
--- ProyectoFinal/ProyectoFinal/Form1.cs
+++ ProyectoFinal/ProyectoFinal/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ProyectoFinal
 {
@@ -20,6 +21,34 @@ namespace ProyectoFinal
         private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
             MessageBox.Show("HOLAA");
+        }
+
+        private void btnShowclient_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Cadena de conexion que solicita el servidor, id, contraseña, base de datos a conectarse
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "s3xo!=am0r";
+            builder.Database = "bdsurticasa";
+            //iniciamos una nueva conexion
+            MySqlConnection conn = new MySqlConnection(builder.ToString());
+            conn.Open();
+            string query = "SELECT * FROM tblcliente ";
+            DataTable dt= new DataTable();
+            MySqlDataAdapter myDataAdapter = new MySqlDataAdapter(query, conn);
+            myDataAdapter.Fill(dt);
+
+            gridView1.Columns.Clear();
+            gridControlClient.DataSource = dt;
+
+            
+        }
+
+        private void btnNewclient_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            frmnewClient frmclientenuevo = new frmnewClient();
+            frmclientenuevo.ShowDialog();
         }
     }
 }
